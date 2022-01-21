42880d1053bca8ef12a6ed1820597d0e87596ce4
Катерина Осадчая <monzolevskaya@gmail.com>
2022-01-21 14:54:34 +0200

    +

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+
+import store from './redux/store';
+
+import App from './App.jsx';
+
+import 'modern-normalize/modern-normalize.css';
+import './App.module.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
